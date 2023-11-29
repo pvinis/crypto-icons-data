383dd5457179b7570b6564cf94bea474ccfcf6f5
Pavlos Vinieratos <pvinis@gmail.com>
2023-11-29 13:52:01 +0200

    something works

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import got from "got"
+import fs from "fs"
+import path from "path"
+
+const downloadImage = async (url: string, filepath: string) => {
+	const imgStream = await got.stream(url)
+	const fileWriter = fs.createWriteStream(filepath)
+	imgStream.pipe(fileWriter)
+	return new Promise((resolve, reject) => {
+		imgStream.on("error", reject)
+		fileWriter.on("finish", resolve)
+	})
+}
+
+const coingecko = got.extend({
+	prefixUrl: "https://api.coingecko.com/api/v3",
+})
+
+const coins: Array<{ id: string; symbol: string; name: string }> = await coingecko
+	.get("coins/list")
+	.json()
+
+for (const coin of coins) {
+	if (
+		fs.existsSync(`data/${coin.symbol}.png`) ||
+		fs.existsSync(`data/${coin.symbol}.svg`) ||
+		fs.existsSync(`data/${coin.symbol}.jpg`)
+	) {
+		console.log(`${coin.symbol} - Skipped. Image exists.`)
+		continue
+	}
+
+	console.log(`${coin.symbol} - Downloading.`)
+
+	const coinData: {
+		id: string
+		symbol: string
+		name: string
+		image: { thumb: string; small: string; large: string }
+	} = await coingecko.get(`coins/${coin.id}`).json()
+
+	const fileUrl = new URL(coinData.image.large)
+	const fileExt = path.extname(fileUrl.pathname)
+	await downloadImage(coinData.image.large, `data/${coin.symbol}${fileExt}`)
+}
