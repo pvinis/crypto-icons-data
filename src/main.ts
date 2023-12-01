5bae1c0e7f2968608e17ec52eaec2afbc3a8438b
Pavlos Vinieratos <pvinis@gmail.com>
2023-12-01 22:57:08 +0200

    more

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,80 @@
-import got from "got"
-import fs from "fs"
 import path from "path"
-import data from "../data/data.json"
+import dataJson from "../data/data.json"
+import symbolIdMapJson from "../data/symbol-id-map.json"
 
-const coingecko = got.extend({
-	prefixUrl: "https://api.coingecko.com/api/v3",
-	headers: { "x-cg-demo-api-key": process.env.COINGECKO_API_KEY },
-	responseType: "json",
-	timeout: { request: 3000 },
-})
+const symbolIdMap: Record<
+	string,
+	Array<{
+		id: string
+		alexa: number | null
+		gecko_rank: number | null
+		gecko_score: number
+		community_score: number
+	}>
+> = symbolIdMapJson
+
+const coingecko = {
+	get: async (url: string) => {
+		return await fetch(`https://api.coingecko.com/api/v3/${url}`, {
+			headers: { "x-cg-demo-api-key": process.env.COINGECKO_API_KEY! },
+		})
+	},
+}
 
 const downloadImage = async (url: string, filepath: string) => {
-	const imgStream = await got.stream(url, {
-		headers: { "x-cg-demo-api-key": process.env.COINGECKO_API_KEY },
-		timeout: { request: 3000 },
+	const img = await fetch(url, {
+		headers: { "x-cg-demo-api-key": process.env.COINGECKO_API_KEY! },
 	})
-	const fileWriter = fs.createWriteStream(filepath)
-	imgStream.pipe(fileWriter)
-	return new Promise((resolve, reject) => {
-		imgStream.on("error", reject)
-		fileWriter.on("finish", resolve)
-	})
+	await Bun.write(filepath, await img.arrayBuffer())
 }
 
 try {
-	const coins: Array<{ id: string; symbol: string; name: string }> = await coingecko
-		.get("coins/list")
-		.json()
+	const coins: Array<{ id: string; symbol: string; name: string }> = await (
+		await coingecko.get("coins/list")
+	).json()
 
-	for (const coin of coins) {
-		if (
-			fs.existsSync(`data/${coin.symbol}.png`) ||
-			fs.existsSync(`data/${coin.symbol}.PNG`) ||
-			fs.existsSync(`data/${coin.symbol}.svg`) ||
-			fs.existsSync(`data/${coin.symbol}.jpg`) ||
-			fs.existsSync(`data/${coin.symbol}.jpeg`)
-		) {
-			console.log(`${coin.symbol} (${coin.id}) - Skipped. Image exists.`)
+	await Bun.write("data/data.json", JSON.stringify(coins, null, 2) + "\n")
+
+	const length = coins.length
+
+	for (const [index, coin] of coins.entries()) {
+		const ids = symbolIdMap[coin.symbol]
+		if (ids !== undefined && ids.some((item) => item.id === coin.id)) {
+			console.log(`${index + 1}/${length} - ${coin.symbol} (${coin.id}) - Skipped. Already exists.`)
 			continue
 		}
+		if (ids === undefined) {
+			symbolIdMap[coin.symbol] = []
+		}
 
-		console.log(`${coin.symbol} (${coin.id}) - Downloading.`)
+		console.log(`${index + 1}/${length} - ${coin.symbol} (${coin.id}) - Fetching..`)
 
 		const coinData: {
 			id: string
 			symbol: string
 			name: string
 			image: { thumb: string; small: string; large: string }
-		} = await coingecko.get(`coins/${coin.id}`).json()
+			public_interest_stats: { alexa_rank: number | null }
+			coingecko_rank: number | null
+			coingecko_score: number
+			community_score: number
+		} = await (await coingecko.get(`coins/${coin.id}`)).json()
 
 		if (coinData.image.large.includes("missing_large")) continue
+
+		symbolIdMap[coin.symbol].push({
+			id: coin.id,
+			alexa: coinData.public_interest_stats.alexa_rank,
+			gecko_rank: coinData.coingecko_rank,
+			gecko_score: coinData.coingecko_score,
+			community_score: coinData.community_score,
+		})
+
+		await Bun.write("data/symbol-id-map.json", JSON.stringify(symbolIdMap, null, 2) + "\n")
+
 		const fileUrl = new URL(coinData.image.large)
 		const fileExt = path.extname(fileUrl.pathname)
-		await downloadImage(coinData.image.large, `data/${coin.symbol}${fileExt}`)
+		await downloadImage(coinData.image.large, `data/icons/large/${coin.id}${fileExt}`)
 	}
 } catch (e) {
 	console.log(e)
